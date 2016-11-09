6161705acc0ac4b171ab0d17538945f76a47f4d4
Erick Adrian Perez <yo@erickperez.com>
2016-11-08 20:41:39 -0700

    Latest and maybe greatest

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var fs = require('fs');
 var ejs = require('ejs');
+var path = require('path');
 var app = express();
+
+app.use(express.static(__dirname + '/assets'));
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
+
 app.get('/contact', function(request, response) {
   response.sendFile(__dirname + '/contact.html');
 });
+
 app.get('/courses', function(request, response) {
   fs.readFile('products.json', 'utf8', function(err, data) {
     var productsPARSED = JSON.parse(data);
@@ -17,6 +23,7 @@ app.get('/courses', function(request, response) {
     response.render('courses.ejs');
   });
 });
+
 app.listen(8000);
 
 console.log('Server running http://localhost:8000');
