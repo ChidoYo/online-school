29e633fa9a70afc41498e70375e0d95ce94d7bc1
Erick Adrian Perez <yo@erickperez.com>
2016-11-12 01:47:36 -0700

    Enabled course page and show data form json file

--- server.js
+++ server.js
@@ -4,25 +4,54 @@ var ejs = require('ejs');
 var path = require('path');
 var app = express();
 
+// I use this to serve static files in the assets folder. Could be CSS files or possible JS files.
 app.use(express.static(__dirname + '/assets'));
 
+// Home page
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
+// Contact page
 app.get('/contact', function(request, response) {
   response.sendFile(__dirname + '/contact.html');
 });
 
+// Courses list
 app.get('/courses', function(request, response) {
-  fs.readFile('products.json', 'utf8', function(err, data) {
-    var productsPARSED = JSON.parse(data);
+
+  fs.readFile('courses.json', 'utf8', function(err, data) {
+
+    var courses = JSON.parse(data);
+
     response.locals = {
-      products: products.products
+      courses: courses.list
     };
+
     response.render('courses.ejs');
   });
 });
+
+// Course
+app.get('/courses/:id', function(request, response) {
+  fs.readFile('courses.json', 'utf8', function(err, data) {
+
+    var courses = JSON.parse(data);
+    var course = courses.list.filter( function(obj) {
+      return obj.id === parseInt(request.params.id);
+    });
+
+    if (course.length)
+      course = course[0];
+    else
+      course = null;
+
+    response.locals = { course: course };
+    response.render('course.ejs');
+ });
+});
+
+
 
 app.listen(8000);
 
